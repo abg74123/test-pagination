8582e0e5bd33642d109e0eb199030fa4364fbf42
abg74123 <bisza_i.d@hotmail.com>
2023-07-13 11:14:03 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "./core/services/product.service";
+import {BehaviorSubject, Observable, tap} from "rxjs";
 
+interface PageEvent {
+  first: number;
+  rows: number;
+  page: number;
+  pageCount: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-pagination';
+
+export class AppComponent implements OnInit{
+
+  products = new BehaviorSubject([])
+  products$:Observable<any> = this.products.asObservable()
+  constructor(private product:ProductService) {
+  }
+
+  ngOnInit(){
+    this.product.getProductList().pipe(tap(
+      res => {
+        this.products.next(res)
+
+      }
+    )).subscribe()
+  }
+
+  first: number = 0;
+  rows: number = 10;
+
+  onPageChange(event: PageEvent,nexPageKey:any) {
+    console.log(event)
+    this.first = event.first;
+    this.rows = event.rows;
+
+    this.product.getProductList({nextPageKey:nexPageKey,limit:event.rows}).pipe(tap(
+      res => {
+        this.products.next(res)
+      }
+    )).subscribe()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableModule } from 'primeng/table';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import { PaginatorModule } from 'primeng/paginator';
 
+const primeng = [
+  TableModule,
+  PaginatorModule
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ...primeng
   ],
   providers: [],
   bootstrap: [AppComponent]
