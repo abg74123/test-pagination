70fd265b87aff217461ac03522b1ad150cea884a
abg74123 <bisza_i.d@hotmail.com>
2023-10-06 12:41:48 +0700

    update log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,14 @@ export class AppComponent implements OnInit{
   destroy:any = new Subject()
   products:any = new BehaviorSubject([])
   products$:Observable<any> = this.products.asObservable()
+
   loading: boolean = true;
   first: number = 0;
   rows: number = 10;
-  totalRecords: number = 10
+  totalRecords: number = this.rows
   // key next page
   nexPageKey = {}
-  // store
+  // store in component
   productsArr:any = []
 
   constructor(private productService:ProductService) {
@@ -34,48 +35,61 @@ export class AppComponent implements OnInit{
   }
 
   onPageChange(event: PageEvent) {
-    console.log("START onPageChange")
-    console.log(event)
+    console.warn("---CTRL | START FUNC onPageChange---")
+    console.warn('---CTRL | EVENT---',event)
 
+    // ^ number first start of page
     this.first = event.first;
+    // ^ number rows
     this.rows = event.rows;
 
     const startSlice = this.first
     const endSlice = this.first + this.rows
 
-    console.log("startSlice => ",startSlice)
-    console.log("endSlice => ",endSlice)
+    console.warn(`---CTRL | GET PAGE [${startSlice}] TO [${endSlice}]---`)
 
+    // ^ slice product start to end
     const sliceProduct = this.productsArr.slice(startSlice, endSlice)
-    console.log({sliceProduct})
 
-    console.log("productsArr => ",this.productsArr.length)
-    console.log("rows => ",this.rows)
+    console.warn('---CTRL | SLICE PRODUCT---',sliceProduct)
 
+    console.warn(`---CTRL | START CHECK CONDITION---`)
     if(sliceProduct.length < this.rows && this.productsArr.length < this.totalRecords){
+      console.warn(`--- ---CTRL | GET NEW PRODUCT---`)
       this.loading = true;
-      console.log("get new product")
+      // ^ get product list
       this.productService.getProductList({compId:'po-1d1f72',nextPageKey:this.nexPageKey,limit:endSlice - this.productsArr.length}).pipe(
         tap(
           (res:any) => {
+            // ^ STEP1 | push product to store
+            console.warn(`--- --- ---CTRL | PUSH NEW PRODCT---`)
             this.productsArr.push(...res.data)
+            console.warn(`--- --- ---CTRL | NEW PRODCT---`,this.productsArr)
+            // ^ STEP2 | save nexPageKey
             this.nexPageKey = res.pagination.nexPageKey
+            // ^ STEP3 | save count all page
             this.totalRecords = res.pagination.count
-            this.products.next(this.productsArr.slice(startSlice, endSlice))
+            // ^ STEP4 | next product new [start] to [end]
+            const productNew = this.productsArr.slice(startSlice, endSlice)
+            console.warn(`--- --- ---CTRL | NEXT PRODUCT NEW---`,productNew)
+            this.products.next(productNew)
             this.loading = false;
+            console.warn("---CTRL | STOP FUNC onPageChange---")
           }
         ),
         takeUntil(this.destroy),
       ).subscribe()
     }else{
-      console.log("!!!not get new product")
+      console.warn(`--- ---CTRL | !!!NOT GET NEW PRODUCT---`)
+      // ^ next product old
       this.products.next(sliceProduct)
+      console.warn("---CTRL | STOP FUNC onPageChange---")
     }
-
   }
 
   ngOnDestroy(): void {
     console.log("destroy component")
+    // ^ unsubscribe
     this.destroy.next()
     this.destroy.complete()
   }
--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -16,19 +16,21 @@ export class ProductService {
   }
 
   getProductList(p?: TGetProductList): Observable<any> {
-
+    console.log("---SERVICE | START FUNC getProductList---");
     let url = `https://xleftpcawg.execute-api.ap-southeast-1.amazonaws.com/beta/product/test/list?compId=${p?.compId}`
 
+    // ^ has limit join string
     if (p && p.limit) {
-      console.log("limit => ",p.limit);
+      console.log("--- ---SERVICE | HAS LIMIT---",p.limit);
       url += `&limit=${p.limit}`
     }
+    // ^ has nextPageKey join string
     if (p && p.nextPageKey && !this.isEmpty(p.nextPageKey)) {
-      console.log("nextPageKey => ",p.nextPageKey);
+      console.log("--- ---SERVICE | HAS NEXT PAGE KEY---",p.limit);
       url += `&key_0=${encodeURIComponent(p.nextPageKey.PK)}&key_1=${encodeURIComponent(p.nextPageKey.SK)}&key_2=${encodeURIComponent(p.nextPageKey.GSI6PK)}&key_3=${encodeURIComponent(p.nextPageKey.GSI6SK)}`
     }
 
-    console.log({url})
+    console.log('---SERVICE | URL---',url)
 
     return this.http.get(url)
   }
